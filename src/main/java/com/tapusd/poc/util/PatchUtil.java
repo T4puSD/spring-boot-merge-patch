a52a413fdccb351b99dedc43d725f94d37990f3c
Tapu Sutradhar <tapu@alphacommerz.com>
2024-07-09 17:03:36 +0600

    feat: implement id mismatch validation
    - this also fix the security issue of updating id while performing patch issue

--- src/main/java/com/tapusd/poc/util/PatchUtil.java
+++ src/main/java/com/tapusd/poc/util/PatchUtil.java
@@ -9,10 +9,13 @@ import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import org.springframework.beans.PropertyAccessor;
+import org.springframework.beans.PropertyAccessorFactory;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class PatchUtil {
     private static final ObjectMapper OBJECT_MAPPER;
@@ -49,11 +52,37 @@ public class PatchUtil {
         Assert.isNull(idNode, "Patch request body should not contains id!");
 
         try {
+            validateIdMismatch(entity, patchRequest);
             JsonNode entityNode = OBJECT_MAPPER.convertValue(entity, JsonNode.class);
             JsonNode updatedJsonNode = OBJECT_MAPPER.readerForUpdating(entityNode).readValue(patchRequest);
             return (E) OBJECT_MAPPER.treeToValue(updatedJsonNode, entity.getClass());
         } catch (IOException ex) {
             throw new IllegalStateException("Failed to apply patch", ex);
+        }
+    }
+
+    public static <E> void validateIdMismatch(E entity, JsonNode patchRequest) {
+        if (Objects.isNull(entity) || Objects.isNull(patchRequest)) {
+            return;
+        }
+
+        JsonNode jsonNode = patchRequest.get("id");
+        if (Objects.nonNull(jsonNode)) {
+
+            String id = extractEntityId(entity);
+            String text = jsonNode.asText();
+            if (!text.equals(id)) {
+                throw new IllegalArgumentException("Patch request id mismatch");
+            }
+        }
+    }
+
+    private static <E> String extractEntityId(E entity) {
+        try {
+            PropertyAccessor accessor = PropertyAccessorFactory.forBeanPropertyAccess(entity);
+            return String.valueOf(accessor.getPropertyValue("id"));
+        } catch (Exception ex) {
+            return null;
         }
     }
 }
